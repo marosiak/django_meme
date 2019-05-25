4a776998c030ed2afb54e04ad51aed2c74599c7d
Maciej Rosiak <orzel1244@gmail.com>
2019-05-25 18:24:39 +0200

    [added] title length validation [added] text-overlay for title

--- meme/views.py
+++ meme/views.py
@@ -15,6 +15,8 @@ def add_meme(request):
         img = request.FILES.get('image', False)
         title = request.POST.get('title', False)
         if title:
+            if len(title) >= 15:
+                return render(request, 'meme/meme_add.html', {'error': 'The title is too long'})
             if img:
                 Meme.objects.create(file=img, title=title, author=request.user)
                 return redirect('meme_list')
